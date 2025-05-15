81fbbdfaf62c5ddae668cbe89209687741d3cfbb
JuneYoung <nietsnie1@gmail.com>
2025-05-15 19:16:36 +0900

    fix: add error handling for empty results in fund info functions

--- fund_insight_engine/fund_data_retriever/fund_info.py
+++ fund_insight_engine/fund_data_retriever/fund_info.py
@@ -6,10 +6,12 @@ def get_fund_price(fund_code, date_ref=None):
     pipeline_price = [
         {'$match': {'일자': date_ref, '펀드코드': fund_code}},
         {'$project': {'_id': 0, '수정기준가': 1}},
-    ]
+    ] 
     cursor = COLLECTION_8186.aggregate(pipeline_price)
-    data = list(cursor)[-1]
-    return data
+    data_list = list(cursor)
+    if not data_list:
+        return {'수정기준가': None}
+    return data_list[-1]
 
 def get_fund_nav(fund_code, date_ref=None):
     date_ref = date_ref or get_latest_date_in_menu8186()
@@ -18,8 +20,10 @@ def get_fund_nav(fund_code, date_ref=None):
         {'$project': {'_id': 0, '순자산': 1}},
     ]
     cursor = COLLECTION_8186.aggregate(pipeline_nav)
-    data = list(cursor)[-1]
-    return data
+    data_list = list(cursor)
+    if not data_list:
+        return {'순자산': None}
+    return data_list[-1]
 
 def get_fund_capital(fund_code, date_ref=None):
     date_ref = date_ref or get_latest_date_in_menu8186()
